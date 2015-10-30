68792f658485e81be526e04e50bd1770903c0637
Magnus Baneryd <magnus.baneryd@episerver.com>
2015-10-30 07:34:14 +0100

    Removed the code from the lab files

--- src/Alloy/modules/_protected/EPiServer.Translate/ClientResources/commands/TranslateNodes.js
+++ src/Alloy/modules/_protected/EPiServer.Translate/ClientResources/commands/TranslateNodes.js
@@ -14,41 +14,4 @@
     _SelectionCommandMixin
 ) {
 
-    return declare([_Command, _SelectionCommandMixin], {
-
-        label: "Translate nodes",
-
-        // Set the icon for the command
-        iconClass: "epi-iconWebsite", //http://ux.episerver.com/#icons
-
-        postscript: function () {
-            this.inherited(arguments);
-
-            // get the store from the store registry
-            this.translateStore = this.translateStore || dependency.resolve("epi.storeregistry").get("epi-translate.translate");
-        },
-
-        _execute: function () {
-            var selectionData = this.get("selectionData"),
-                sender = this;
-            var contentLink = selectionData.contentLink;
-
-            this.translateStore.executeMethod("Translate", contentLink).then(function (projectId) {
-                // reload the children of the selected item
-                topic.publish("/epi/cms/contentdata/childrenchanged", selectionData);
-                // reload the selected item
-                topic.publish("/epi/cms/contentdata/updated", selectionData);
-
-                // load the project overview for the newly created project
-                topic.publish("/epi/shell/context/request", { uri: "epi.cms.project:///" + projectId }, { sender: sender });
-            }).otherwise(function() {
-                console.log("could not translate the nodes");
-            });
-        },
-
-        _onModelChange: function () {
-            // enable the command if there is a selection in the tree
-            this.set("canExecute", !!this.get("selectionData"));
-        }
-    });
 });
--- src/Alloy/modules/_protected/EPiServer.Translate/ZapireNotificationFormatter.cs
+++ src/Alloy/modules/_protected/EPiServer.Translate/ZapireNotificationFormatter.cs
@@ -7,29 +7,5 @@ namespace EPiServer.Translate
     /// <summary>
     /// We are using a BETA API
     /// </summary>
-    [ServiceConfiguration(typeof(INotificationFormatter))]
-    public class ZapireNotificationFormatter : INotificationFormatter
-    {
-        public IEnumerable<FormatterNotificationMessage> FormatMessages(IEnumerable<FormatterNotificationMessage> notifications, string recipient, NotificationFormat format, string notificationChannelName)
-        {
-            // we do not want to change the messages, so we just return them as they are
-            // but you have the possibility to group several messages into one if you would like to
-            return notifications;
-        }
-
-        public const string ChannelName = "translate";
-
-        /// <summary>
-        /// The name of the formatter
-        /// </summary>
-        public string FormatterName { get { return "zapire"; } }
-
-        /// <summary>
-        /// Specifies which channels the formatter supports.
-        /// </summary>
-        public IEnumerable<string> SupportedChannelNames
-        {
-            get { return new[] { ChannelName }; }
-        }
-    }
+    
 }
